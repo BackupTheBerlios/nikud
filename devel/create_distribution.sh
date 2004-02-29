a8294f8b214ed9eabce6e2121e4b8687d9dadbf0
itail <itail>
2004-02-29 09:02:48 +0000

    *** empty log message ***

--- devel/create_distribution.sh
+++ devel/create_distribution.sh
@@ -4,24 +4,23 @@ rm -fr tmp
 cd nikud
 make clean
 cd ..
-mkdir nikud_source
+mkdir nikud_source_$1
+rm -fr out
 mkdir out
-cp -fr nikud/* nikud_source/
-cd nikud_source
-find . -iname CVS |xargs rm -fr
-cd ..
-tar -cjvf nikud_source.tar.bz2 nikud_source/
-mv nikud_source.tar.bz2 out/
+cp -fr nikud/* nikud_source_$1/
+cd nikud_source_$1 && find . -iname CVS |xargs rm -fr && cd ..
+tar -cjvf nikud_source_$1.tar.bz2 nikud_source_$1/
+mv nikud_source_$1.tar.bz2 out/
 rm -fr ~/texmf
-cd nikud_source/
+cd nikud_source_$1/
 make
 make install
 cd ..
 pushd .
 cd
-mv texmf nikud_texmf
-tar -cjvf nikud_texmf.tar.bz2 nikud_texmf/
+mv texmf nikud_texmf_$1
+tar -cjvf nikud_texmf_$1.tar.bz2 nikud_texmf_$1/
 popd
-mv ~/nikud_texmf.tar.bz2 out
+mv ~/nikud_texmf_$1.tar.bz2 out
 rm -fr tmp
-rm -fr nikud_source
+rm -fr nikud_source_$1
