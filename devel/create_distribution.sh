2ee23e31902ac2f755cb48135a98d54e9859e2d0
itail <itail>
2004-10-29 10:26:22 +0000

    creating rpm automatically

--- devel/create_distribution.sh
+++ devel/create_distribution.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ $# -ne 1 ]
+then
+  echo "Usage: `basename $0` <version number> (like 0.108.33)"
+  exit 1
+fi
+
 rm -fr tmp
 cd nikud
 make clean
@@ -23,3 +29,9 @@ popd
 mv ~/nikud_texmf-$1.tar.bz2 out
 rm -fr tmp
 rm -fr nikud-$1
+
+./create_rpm.sh $1
+
+echo ========================================================
+echo If everything went well, products are in ./out directory
+echo ========================================================
--- devel/create_rpm.sh
+++ devel/create_rpm.sh
@@ -1,20 +1,27 @@
 #/bin/sh
 
-NEWROOT=~/crust_newroot
-VERSION=0.100
-RELEASE=1
+if [ $# -ne 1 ]
+then
+  echo "Usage: `basename $0` <version number> (like 0.108.33)"
+  exit 1
+fi
 
-rm -fr $NEWROOT
-mkdir -p $NEWROOT
+# patch the template spec file and create the real spec file
+echo %define version $1 > ./nikud_rpm.spec
+cat ./nikud_rpm.spec.template >> ./nikud_rpm.spec
 
-crust --init --copy $NEWROOT
-cp out/nikud-$VERSION.tar.bz2 $NEWROOT/build
-pushd .
-cd $NEWROOT/build
-tar -xjvf nikud-$VERSION.tar.bz2
-cd nikud-$VERSION
-make -f Makefile.crust
-popd
-sudo /usr/sbin/chroot $NEWROOT make -C /build/nikud-$VERSION -f /build/nikud-$VERSION/Makefile.crust install
-sudo chown -R itai $NEWROOT
-crust --makerpm --name nikud --version $VERSION --release $RELEASE --group Publishing --license "GNU GPL" --sum "Hebrew Culmus fonts in LaTeX with Nikud" --desc "Use Hebrew Culmus fonts in LaTeX with Hebrew vowel-points" --post /home/itai/nikud_devel/devel/rpm_post_install.sh --postun /home/itai/nikud_devel/devel/rpm_post_uninstall.sh $NEWROOT
+cp -f ./.rpmmacros ~/
+rm -fr ~/rpms/
+mkdir -p ~/rpms/nikud
+mkdir -p ~/rpms/RPMS
+mkdir -p ~/rpms/tmp
+
+cp -f ./out/nikud-*.tar.bz2 ~/rpms/nikud/
+
+rpmbuild -ba --target noarch nikud_rpm.spec
+cp -f ~/rpms/RPMS/* ./out/
+
+rm -f ./nikud_rpm.spec
+rm -f ~/.rpmmacros
+rm -fr ~/rpms/
+
